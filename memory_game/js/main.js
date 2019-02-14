b0f111701ac75392bd866e2fa9f77a880ed57c07
Elizabeth Moy <elizabethmoy@Elizabeths-MacBook-Pro.local>
2019-02-14 05:27:12 -0500

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -2,18 +2,24 @@ console.log("Up and running!");
 
 var cards = ["queen", "queen", "king", "king"];
 var cardsInPlay = [];
-var cardOne = cards[2];
-cardsInPlay.push("cardOne");
-console.log("User flipped king");
 
-var cardTwo = cards[3];
-cardsInPlay.push("cardTwo");
-console.log("User flipped king");
-
-if (cardsInPlay.length === 2) {
+var checkForMatch = function () {
+	if (cardsInPlay.length === 2) {
 	//check if length of array is equal to 2
-	if (cardsInPlay[2] === cardsInPlay[3])
-	alert("You found a match!");
-	else
-	alert("Sorry try again.");
+
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+	console.log("You found a match!");
+	} else {
+	console.log("Sorry, try again.");
 	}
+}
+};
+
+var flipCard = function (cardId) {
+	console.log("User flipped " + cards[cardId]);
+	cardsInPlay.push(cards[cardId]);
+checkForMatch();
+};
+
+flipCard(0);
+flipCard(2);
